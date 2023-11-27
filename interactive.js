aa8c81a5a5cb2740b44dfa1ac6457714dea7a206
Pablo Sáenz de Tejada <99508682+psaenzdetejada@users.noreply.github.com>
2023-11-27 12:46:10 +0100

    Add files via upload

---
+++ interactive.js
@@ -0,0 +1,45 @@
+import {  FilterUpdateType } from 'https://public.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js';
+  $(document).ready(() => {
+
+
+function filterState() {
+    let viz = document.getElementById("tableauViz2");
+    console.log( viz);
+    let sheet = viz.workbook.activeSheet;
+    const saleMap = sheet.worksheets.find((ws) => ws.name == "MapaporPais");
+    saleMap.applyFilterAsync("Country", ["Argentina"], FilterUpdateType.Replace);
+};
+
+
+function clearState() {
+    let viz = document.getElementById("tableauViz2");
+    console.log( viz);
+    let sheet = viz.workbook.activeSheet;
+    const saleMap = sheet.worksheets.find((ws) => ws.name == "SaleMap");
+    saleMap.clearFilterAsync("State");
+};
+
+function unDo() {
+    let viz = document.getElementById("tableauViz2");
+    console.log( viz);
+    viz.undoAsync();
+};
+
+
+$("#filterButton").click(() => {
+    filterState();
+
+});
+
+$("#clearButton").click(() => {
+    clearState();
+
+});
+
+$("#undoButton").click(() => {
+    unDo();
+
+});
+
+
+})
